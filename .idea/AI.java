cf974bd6d1fce99835fdb903475b98f587891aff
Maksym019 <maksym.frolov.ch@gmail.com>
2024-01-27 22:21:42 +0100

    Version1.2

---
+++ .idea/AI.java
@@ -0,0 +1,116 @@
+public class AI {
+    public static void makeOneComputerStep(String[][] pole) { //The main method in this Class
+        String myNum = "X";
+        Computer: while (true) {
+            if (howMuchPoints(pole) == 0) {
+                pole[(int)(Math.random() * 3)][(int)(Math.random() * 3)] = "X";
+                break;
+            } else if (howMuchPoints(pole) == 1) {
+                int x, y;
+                while(true) {
+                    x = (int)(Math.random() * 3);
+                    y = (int)(Math.random() * 3);
+                    if (pole[x][y].equals(" ")) {
+                        pole[x][y] = myNum;
+                        break Computer;
+                    }
+                }
+            } else if (howMuchPoints(pole) >= 2) {
+                if (((pole[0][0].equals("O")) && (pole[0][1].equals("O"))) && (pole[0][2].equals(" "))) {//1
+                    pole[0][2] = myNum;
+                    break;
+                } else if (((pole[0][0].equals("O")) && pole[0][2].equals("O")) && (pole[0][1].equals(" "))) {//2
+                    pole[0][1] = myNum;
+                    break;
+                } else if (((pole[0][1].equals("O")) && pole[0][2].equals("O")) && (pole[0][0].equals(" "))) {//3
+                    pole[0][0] = myNum;
+                    break;
+                } else if (((pole[1][0].equals("O")) && pole[1][1].equals("O")) && (pole[1][2].equals(" "))) {//4
+                    pole[1][2] = myNum;
+                    break;
+                } else if (((pole[1][0].equals("O")) && pole[1][2].equals("O")) && (pole[1][1].equals(" "))) {//5
+                    pole[1][1] = myNum;
+                    break;
+                } else if (((pole[1][1].equals("O")) && pole[1][2].equals("O")) && (pole[1][0].equals(" "))) {//6
+                    pole[1][0] = myNum;
+                    break;
+                } else if (((pole[2][0].equals("O")) && pole[2][1].equals("O")) && (pole[2][2].equals(" "))) {//7
+                    pole[2][2] = myNum;
+                    break;
+                } else if (((pole[2][0].equals("O")) && pole[2][2].equals("O")) && (pole[2][1].equals(" "))) {//8
+                    pole[2][1] = myNum;
+                    break;
+                } else if (((pole[2][1].equals("O")) && pole[2][2].equals("O")) && (pole[2][0].equals(" "))) {//9
+                    pole[2][0] = myNum;
+                    break;
+                } else if (((pole[0][0].equals("O")) && pole[1][0].equals("O")) && (pole[2][0].equals(" "))) {//10
+                    pole[2][0] = myNum;
+                    break;
+                } else if (((pole[0][0].equals("O")) && pole[2][0].equals("O")) && (pole[1][0].equals(" "))) {//11
+                    pole[1][0] = myNum;
+                    break;
+                } else if (((pole[1][0].equals("O")) && pole[2][0].equals("O")) && (pole[0][0].equals(" "))) {//12
+                    pole[0][0] = myNum;
+                    break;
+                } else if (((pole[0][1].equals("O")) && pole[1][1].equals("O")) && (pole[2][1].equals(" "))) {//13
+                    pole[2][1] = myNum;
+                    break;
+                } else if (((pole[0][1].equals("O")) && pole[2][1].equals("O")) && (pole[1][1].equals(" "))) {//14
+                    pole[1][1] = myNum;
+                    break;
+                } else if (((pole[1][1].equals("O")) && pole[2][1].equals("O")) && (pole[0][1].equals(" "))) {//15
+                    pole[0][1] = myNum;
+                    break;
+                } else if (((pole[0][2].equals("O")) && pole[1][2].equals("O")) && (pole[2][2].equals(" "))) {//16
+                    pole[2][2] = myNum;
+                    break;
+                } else if (((pole[0][2].equals("O")) && pole[2][2].equals("O")) && (pole[1][2].equals(" "))) {//17
+                    pole[1][2] = myNum;
+                    break;
+                } else if (((pole[1][2].equals("O")) && pole[2][2].equals("O")) && (pole[0][2].equals(" "))) {//18
+                    pole[0][2] = myNum;
+                    break;
+                } else if (((pole[0][0].equals("O")) && pole[1][1].equals("O")) && (pole[2][2].equals(" "))) {//19
+                    pole[2][2] = myNum;
+                    break;
+                } else if (((pole[0][0].equals("O")) && pole[2][2].equals("O")) && (pole[1][1].equals(" "))) {//20
+                    pole[1][1] = myNum;
+                    break;
+                } else if (((pole[1][1].equals("O")) && pole[2][2].equals("O")) && (pole[0][0].equals(" "))) {//21
+                    pole[0][0] = myNum;
+                    break;
+                } else if (((pole[0][2].equals("O")) && pole[1][1].equals("O")) && (pole[2][0].equals(" "))) {//22
+                    pole[2][0] = myNum;
+                    break;
+                } else if (((pole[2][0].equals("O")) && pole[0][2].equals("O")) && (pole[1][1].equals(" "))) {//23
+                    pole[1][1] = myNum;
+                    break;
+                } else if (((pole[2][0].equals("O")) && pole[1][1].equals("O")) && (pole[0][2].equals(" "))) {//24
+                    pole[0][2] = myNum;
+                    break;
+                } else {
+                    for (byte i = 0; i < pole.length; i++) {
+                        for (byte j = 0; j < pole[i].length; j++) {
+                            if (pole[i][j].equals(" ")) {
+                                pole[i][j] = myNum;
+                                break Computer;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private static byte howMuchPoints(String[][] pole) {
+        byte count = 0;
+        for (byte i = 0; i < pole.length; i++) {
+            for (byte j = 0; j < pole.length; j++) {
+                if (pole[i][j].equals("O")) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
---
+++ .idea/KrestikiNoliki.java
@@ -0,0 +1,130 @@
+import java.util.Scanner;
+
+public class KrestikiNoliki {
+    private static String[][] Pole = new String[3][3]; //The main TikTakToe board/
+    private static boolean isGameFinish = false; //variable that determines whether the game is over.
+    private static boolean Choise = (((int)(Math.random() * 2)) == 1);
+
+    public static void main(String[] args) { //The main method that calls all other methods in order
+        for (byte i = 0; i < Pole.length; i++) {
+            for (byte j = 0; j < Pole[i].length; j++) {
+                Pole[i][j] = " ";
+            }
+        }
+
+        while (!isGameFinish) { //The main loop, which calls the necessary methods in
+            if (Choise) {       //order depending on who is walking.
+                System.out.println("Now is your step");
+                printBoard();
+                makeOnePlayerStep();
+                isWinPosition();
+                hasSpaseInBoard();
+                printBoard();
+                Choise = false;
+            } else if (!Choise) {
+                System.out.println("Now computer make one step");
+                AI.makeOneComputerStep(Pole);
+                isWinPosition();
+                hasSpaseInBoard();
+                printBoard();
+                Choise = true;
+            }
+        }
+    }
+
+    private static void makeOnePlayerStep() {
+        Scanner console = new Scanner(System.in);
+        boolean isMove = false;
+        System.out.println("Please, print two numbers form 0 to 2");
+        byte x, y;
+        while (!isMove) {
+            x = console.nextByte();
+            y = console.nextByte();
+            if (Pole[x][y].equals(" ")) {
+                Pole[x][y] = "O";
+                isMove = true;
+            } else {
+                System.out.println("This cell is closed, please write another one");
+            }
+        }
+    }
+
+    private static void printBoard() {
+        System.out.println("-------------");
+        for (String[] a: Pole) {
+            System.out.print("| ");
+            for (String b: a) {
+                System.out.print(b + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+
+    private static void isWinPosition() {
+        if (Pole[0][0].equals("O") && (Pole[0][1].equals("O") && Pole[0][2].equals("O"))) {
+            System.out.println("You win!");
+            isGameFinish = true;
+        } else if (Pole[1][0].equals("O") && (Pole[1][1].equals("O") && Pole[1][2].equals("O"))) {
+            System.out.println("You win!");
+            isGameFinish = true;
+        } else if (Pole[2][0].equals("O") && (Pole[2][1].equals("O") && Pole[2][2].equals("O"))) {
+            System.out.println("You win!");
+            isGameFinish = true;
+        } else if (Pole[0][0].equals("O") && (Pole[1][0].equals("O") && Pole[2][0].equals("O"))) {
+            System.out.println("You win!");
+            isGameFinish = true;
+        } else if (Pole[0][1].equals("O") && (Pole[1][1].equals("O") && Pole[2][1].equals("O"))) {
+            System.out.println("You win!");
+            isGameFinish = true;
+        } else if (Pole[0][2].equals("O") && (Pole[1][2].equals("O") && Pole[2][2].equals("O"))) {
+            System.out.println("You win!");
+            isGameFinish = true;
+        } else if (Pole[0][0].equals("O") && (Pole[1][1].equals("O") && Pole[2][2].equals("O"))) {
+            System.out.println("You win!");
+            isGameFinish = true;
+        } else if (Pole[0][2].equals("O") && (Pole[1][1].equals("O") && Pole[2][0].equals("O"))) {
+            System.out.println("You win!");
+            isGameFinish = true;
+        } else if (Pole[0][0].equals("X") && (Pole[0][1].equals("X") && Pole[0][2].equals("X"))) {
+            System.out.println("Computer win!");
+            isGameFinish = true;
+        } else if (Pole[1][0].equals("X") && (Pole[1][1].equals("X") && Pole[1][2].equals("X"))) {
+            System.out.println("Computer win!");
+            isGameFinish = true;
+        } else if (Pole[2][0].equals("X") && (Pole[2][1].equals("X") && Pole[2][2].equals("X"))) {
+            System.out.println("Computer win!");
+            isGameFinish = true;
+        } else if (Pole[0][0].equals("X") && (Pole[1][0].equals("X") && Pole[2][0].equals("X"))) {
+            System.out.println("Computer win!");
+            isGameFinish = true;
+        } else if (Pole[0][1].equals("X") && (Pole[1][1].equals("X") && Pole[2][1].equals("X"))) {
+            System.out.println("Computer win!");
+            isGameFinish = true;
+        } else if (Pole[0][2].equals("X") && (Pole[1][2].equals("X") && Pole[2][2].equals("X"))) {
+            System.out.println("Computer win!");
+            isGameFinish = true;
+        } else if (Pole[0][0].equals("X") && (Pole[1][1].equals("X") && Pole[2][2].equals("X"))) {
+            System.out.println("Computer win!");
+            isGameFinish = true;
+        } else if (Pole[0][2].equals("X") && (Pole[1][1].equals("X") && Pole[2][0].equals("X"))) {
+            System.out.println("Computer win!");
+            isGameFinish = true;
+        }
+    }
+
+    private static void hasSpaseInBoard() {
+        byte Point = 0;
+        loop1: for (byte i = 0; i < Pole.length; i++) {
+            for (byte j = 0; j < Pole[i].length; j++) {
+                if (Pole[i][j].equals(" ")) {
+                    Point++;
+                }
+            }
+        }
+        if (Point == 0) {
+            System.out.println("There is no free space in board, nobody win!");
+            isGameFinish = true;
+        }
+    }
+}
